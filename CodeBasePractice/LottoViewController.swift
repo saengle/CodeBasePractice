099d58c47f0162666be2ca33d429f9ed47bef490
saengle <zohv817@gmail.com>
2024-06-05 18:03:50 +0900

    Initial Commit

---
+++ CodeBasePractice/LottoViewController.swift
@@ -0,0 +1,23 @@
+//
+//  LottoViewController.swift
+//  CodeBasePractice
+//
+//  Created by 쌩 on 6/5/24.
+//
+
+import UIKit
+
+
+
+class LottoViewController: UIViewController {
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = .white
+    }
+}
+
+
